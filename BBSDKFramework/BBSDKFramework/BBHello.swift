8d4b43862d5448db7594870599b983646967873f
hmily-xinxiang@163.com <hmily-xinxiang@163.com>
2016-06-16 10:41:32 +0800

    iOS sdk framework

---
+++ BBSDKFramework/BBSDKFramework/BBHello.swift
@@ -0,0 +1,15 @@
+//
+//  BBHello.swift
+//  BBSDKFramework
+//
+//  Created by Bei Wang on 6/15/16.
+//  Copyright © 2016 Begodya. All rights reserved.
+//
+
+import Foundation
+
+public class BBHello {
+    public class func sayHello() {
+        print("Hello BB")
+    }
+}
---
+++ BBSDKFramework/BBSDKFramework/BBSDKFramework.h
@@ -0,0 +1,19 @@
+//
+//  BBSDKFramework.h
+//  BBSDKFramework
+//
+//  Created by Bei Wang on 6/15/16.
+//  Copyright © 2016 Begodya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBSDKFramework.
+FOUNDATION_EXPORT double BBSDKFrameworkVersionNumber;
+
+//! Project version string for BBSDKFramework.
+FOUNDATION_EXPORT const unsigned char BBSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBSDKFramework/PublicHeader.h>
+
+
